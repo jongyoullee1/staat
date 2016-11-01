11a123fac4443911858e16bba94619040cc81fcb
Phionx <shanjha.usa@gmail.com>
2016-11-01 11:36:38 -0400

    permissions

--- info.c
+++ info.c
@@ -5,31 +5,43 @@
 #include <sys/types.h>
 #include <time.h>
 #include <string.h>
+#include <errno.h>
 
 
 void info(char* fp){
 	struct stat *ans = (struct stat *)malloc(sizeof(struct stat));
-	stat(fp, ans);
+	if(stat(fp, ans) == -1) printf("Error #%d: %s", errno, strerror(errno));
 	long s = (long)ans->st_size;
 	char *a;
-	if (s < 1000){
+
+	if (s < 1024){
 		strcpy(a, "B");
-	} else if ( s > 1000 && s <1000000 ){
+	} else if ( s > 1024 && s <1024*1024 ){
 		strcpy(a, "KB");
-		s = s%1000;
-	} else if (  s > 1000000 && s < 1000000000 ){
+		s = s%1024;
+	} else if (  s > 1024*1024 && s < 1024*1024*1024){
 		strcpy(a, "MB");
-		s = s%1000000;
+		s = s%1024*1024;
 	} else {
 		strcpy(a, "GB");
-		s = s%1000000000;
-	}
+		s = s%1024*1024*1024;
+	}	
+	/**/
+	int mode =  ans->st_mode & 0777;
+	printf("File Name: %s\nFile Size: %ld%s\nMode: %d\nTime of last access: %s",fp, s, a ,mode,  ctime(&(ans->st_atime)));
 
-	
+	char perm[10];
+	int i = 8;
+	while (mode) {
+    perm[i] = (mode % 2) ? 'x' : '-'; mode /= 2; i--;
+    perm[i] = (mode % 2) ? 'w' : '-'; mode /= 2; i--;
+    perm[i] = (mode % 2) ? 'r' : '-'; mode /= 2; i--;
+  }
+	perm[9] = 0;
 
 
+	printf("\nFile Permissions: \t%s\n", perm);
 
-	printf("File Name: %s\nFile Size: %ld%s\nMode: %o\nTime of last access: %s",fp, s, a , ans->st_mode, ctime(&(ans->st_atime)));
 }
 
 int main() {
